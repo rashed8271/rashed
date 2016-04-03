42e49291fff09f5e014253dd97bd553ef9920774
rashed <rashed.hacker8271@gmail.com>
2016-04-03 09:13:45 -0700

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -45,7 +45,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'@QuickGuardTEAM'
+  text = text..'ραяα∂υχ'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, ' Username not found.', ok_cb, false)
@@ -84,7 +84,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'@QuickGuardTEAM'
+  text = text..'ραяα∂υχ'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'id not found.\nuse : /info @username', ok_cb, false)
@@ -123,7 +123,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'@QuickGuardTEAM'
+  text = text..'ραяα∂υχ'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -137,7 +137,7 @@ local function run(msg, matches)
   local hash = 'usecommands:'..msg.from.id..':'..msg.to.id
   redis:incr(hash)
   if not is_sudo(msg) then
-    return "کونده مگه تو بابامی؟"
+    return "فقط سودو قادر به انجام این کار است"
   end
   local receiver = get_receiver(msg)
   local Reply = msg.reply_id
@@ -195,7 +195,7 @@ local function run(msg, matches)
 	 text = text..'Group name : '..msg.to.title..'\n'
      text = text..'Group ID : '..msg.to.id
     end
-	text = text..'\n\n@QuickGuardTEAM'
+	text = text..'\n\nραяα∂υχ'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
