58253e185946b5733325f750f02c9c144146511b
rashed <rashed.hacker8271@gmail.com>
2016-04-03 09:19:10 -0700

    Update share_contact.lua

--- plugins/share_contact.lua
+++ plugins/share_contact.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "0090", "کص", "ننت برو پخش کن خخخ", ok_cb, false)
+send_contact(get_receiver(msg), "+1 253 252 8261", "ραяα∂υχ", ok_cb, false)
 end
 
 return {
